e46f0577f7f846db6836aba4050b639651321003
richardvuong <richard.vuong@outlook.com>
2016-04-15 12:07:02 +0100

    Added files via upload

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 15 11:34:43 2016
+
+@author: richard.vuong
+"""
+
+print('--------------------')
+print('  VERY POLITE APP   ')
+print('--------------------')
+
+userName = input('What is your name? ')
+print('Nice to meet you ' + userName +'!')
