f78234f13322e2cbb765dcbb5e33ca07b56cb6fb
richardvuong <richard.vuong@outlook.com>
2016-05-24 10:48:31 +0100

    Add files via upload

---
+++ cat_service.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 23 10:55:53 2016
+
+@author: richard.vuong
+"""
+
+import os
+import shutil
+import requests
+
+
+def get_cat(folder, name):
+    url = 'http://consuming-python-services-api.azurewebsites.net/cats/random'
+    data = get_data_from_url(url)
+    save_image(folder, name, data)
+
+
+def get_data_from_url(url):
+    response = requests.get(url, stream=True)
+    return response.raw
+
+
+def save_image(folder, name, data):
+    file_name = os.path.join(folder, name + '.jpg')
+    with open(file_name, 'wb') as fout:
+        shutil.copyfileobj(data, fout)
